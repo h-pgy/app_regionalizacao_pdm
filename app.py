f7b713c4fec1639f9e9ca3c4db7209151067215e
h-pgy <hpougy@gmail.com>
2021-07-30 14:42:12 -0300

    melhorando o estilo

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
 import plotly.express as px
 import random
 from utils import (metas_por_secretaria, filtrar_por_meta, 
     decidir_plot, prep_shapefiles, prep_data)
+from front_end import simple_jumbotron, navbar
 
 #abre os dados utilizados no dash
 df = prep_data()
@@ -18,46 +20,58 @@ metas = df['numero_meta'].unique()
 
 
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = [dbc.themes.LUX]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.title = 'Regionalização PDM'
 server = app.server
 
 app.layout = html.Div([
-    html.H1('Regionalização: Programa de Metas 2021-2024'),
-    dcc.Graph(id="choropleth"),
-    html.Div(children=[
-    dcc.Dropdown(
-        id='dropdown-secretarias',
-        options=[
-            {'label': secretaria, 'value': secretaria}
-            for secretaria in secretarias
+    #cabeçalho
+    html.Div([
+    navbar,
+    ]),
+    #explicação app
+    html.Div([
+        simple_jumbotron,
+        #botoes
+        html.Div(
+            children=[
+                html.H6('Escolha a Secretaria Responsável'),
+                dcc.Dropdown(
+                    id='dropdown-secretarias',
+                    options=[
+                        {'label': secretaria, 'value': secretaria}
+                        for secretaria in secretarias
+                    ],
+                    value='',
+                    placeholder="Secretaria Responsável",
+                    style={'padding':'5px'}
+                ),
+                html.H6('Escolha a meta que deseja visualizar'),
+                dcc.Dropdown(
+                    id='dropdown-metas',
+                    options=[
+                        {'label': meta, 'value': meta}
+                        for meta in metas
+                    ],
+                    value=random.choice(metas),
+                    placeholder="Número da meta",
+                    style={'padding':'5px'}
+                    
+                )
+            ],
+            style={'float':'left', 'width' : '50%', 'padding':'50px'}
+            ), 
+        ], 
+        className='flex-container',
+        ),
+    html.Div([
+        dcc.Graph(id="choropleth", style = {'display' : 'inline-block', 'width': '80%', 'float' : 'left'})
         ],
-        value='',
-        placeholder="Secretaria Responsável",
-        style={'display': 'inline-block',
-        'width' : '40%',
-        'height': '20px',
-        'padding': '10px',
-        'text-align': 'center',},
+    className='flex-container'
     ),
-    dcc.Dropdown(
-        id='dropdown-metas',
-        options=[
-            {'label': meta, 'value': meta}
-            for meta in metas
-        ],
-        value=random.choice(metas),
-        placeholder="Número da meta",
-        style={'display': 'inline-block',
-        'width' : '40%',
-        'height': '20px',
-        'padding': '10px',
-        'text-align': 'center',
-        'position': 'relative'},
-    ),
-    ])
+    
 ])
 
 @app.callback(
---
+++ front_end.py
@@ -0,0 +1,40 @@
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+
+
+simple_jumbotron = dbc.Jumbotron(
+    [
+        html.H2("Regionalização: Programa de Metas 2021-24", className="display-5"),
+        html.P(
+            "Aplicação que permite visualizar a regionalizaçaõ das Metas do "
+            "Programa de Metas 2021-2024.",
+            className="lead",
+        ),
+        html.Hr(className="my-2"),
+        html.P(
+            "Secretaria Executiva de Planejamento e Entregas Prioritárias"
+            "da Secretaria de Governo da Prefeitura Municipal de São Paulo"
+        ),
+    ],
+    style = {'height' : '5', 'float' : 'left'}
+)
+
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink("PDM 2021-2024", href="#")),
+        dbc.DropdownMenu(
+            children=[
+                dbc.DropdownMenuItem("Nossos Portais", header=True),
+                dbc.DropdownMenuItem("Portal SEPEP", href="#"),
+                dbc.DropdownMenuItem("Portal Prefeitura", href="#"),
+            ],
+            nav=True,
+            in_navbar=True,
+            label="Mais",
+        ),
+    ],
+    brand="SEPEP",
+    color="primary",
+    dark=True,
+)
